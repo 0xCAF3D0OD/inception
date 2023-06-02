576361aa186f84a9cd9387d8657da2b272c7840c
dino <kevin.dinocera@protonmail.com>
2023-06-02 20:48:13 +0200

    add docker-compose.yml

---
+++ test/test.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int i = 0;
+	for (; i < 10; ++i);
+	printf("j'ai lancer le fichier avec le dockerfile et i vaut [%d]\n", i);
+	printf("modifs\n");
+	return (0);
+}

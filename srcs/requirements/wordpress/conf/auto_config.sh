be27539eb169be50b43c16a5684de5019e414dec
dino <kevin.dinocera@protonmail.com>
2023-06-12 11:00:37 +0200

    resolve macbook conflict

--- srcs/requirements/wordpress/conf/auto_config.sh
+++ srcs/requirements/wordpress/conf/auto_config.sh
@@ -10,10 +10,15 @@ if [ ! -e /var/www/wordpress/wp-config.php ]; then
 #	wp user create --allow-root --role=author $USER1_LOGIN $USER1_MAIL --user_pass=$USER1_PASS --path='/var/www/wordpress' >> /log.txt
 #
 #fi
-	cd /var/www/wordpress/ --allow-root
-	wp core download --allow-root --locale=fr_FR
-	wp config create --path='/var/www/wordpress' --allow-root	--dbname=$SQL_DATABASE --dbuser=$SQL_USER --dbpass=$SQL_PASSWORD --dbhost=mariadb:3306
+	wp core download --allow-root --path='/var/www/wordpress' --locale=fr_FR
+	wp config create --allow-root	--path='/var/www/wordpress' --dbname=$SQL_DATABASE --dbuser=$SQL_USER --dbpass=$SQL_PASSWORD --dbhost=mariadb:3306
 
-	wp core install --url=$DOMAIN_NAME --title=$SITE_TITLE --admin_user=$ADMIN_USER --admin_password=$ADMIN_PASSWORD  --admin_email=$ADMIN_EMAIL --allow-root --path='/var/www/wordpress'
-#	wp user create --allow-root --role=author $USER1_LOGIN $USER1_MAIL --user_pass=$USER1_PASS --path='/var/www/wordpress' >> /log.txt
-	fi
+	wp core install --allow-root --path='/var/www/wordpress' --url=$DOMAIN_NAME --title=$SITE_TITLE --admin_user=$ADMIN_USER --admin_password=$ADMIN_PASSWORD  --admin_email=$ADMIN_EMAIL
+	wp user create --allow-root --path='/var/www/wordpress' >> /log.txt --role=author $USER1_LOGIN $USER1_MAIL --user_pass=$USER1_PASS
+fi
+
+if [ ! -d /run/php ]; then
+	mkdir ./run/php
+fi
+
+/usr/sbin/php-fpm7.3 -F

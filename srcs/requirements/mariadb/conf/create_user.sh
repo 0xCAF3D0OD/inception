633aad989c8c100d79bb922bfa45216a82338a2a
dino <kevin.dinocera@protonmail.com>
2023-06-22 19:17:12 +0200

    add mariadbconf and script

---
+++ srcs/requirements/mariadb/conf/create_user.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+source /Users/dino/Documents/Cursus_42/inception/srcs/.env
+
+echo "database is =${SQL_DATABASE}"
+echo "user is =${SQL_USER}"
+echo "pass is =${SQL_PASSWORD}"
+echo "pass root is =${SQL_ROOT_PASSWORD}"
+
+service mysql start;
+
+mysql -u root -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
+mysql -u root -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
+mysql -u root -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`{SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
+mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
+mysql -u root  -e "FLUSH PRIVILEGES;"
+mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
+
+exec mysqld_safe

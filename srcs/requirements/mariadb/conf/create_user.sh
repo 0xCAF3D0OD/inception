5689864dae1636e95384e288720a88ed94ae0699
hack10101101 <kevin.dinocera@protonmail.com>
2023-06-27 12:21:58 +0200

    modification script

--- srcs/requirements/mariadb/conf/create_user.sh
+++ srcs/requirements/mariadb/conf/create_user.sh
@@ -6,16 +6,14 @@ cat .exist 2>/dev/null
 if [ $? -ne 0 ]; then
 
   # Run the server in the background and wait for it to start up before creating the database
-  mysql_install_db --datadir=/var/lib/mysql \
-    --auth-root-authentication-method=normal
+  mysql_install_db 
 
   # Give permissions to the database for the user mysql
   chown -R mysql:mysql /var/lib/mysql
   chown -R mysql:mysql /run/mysqld
 
   # Start the server
-  ls -la /var/lib/mysql
-  mysqld_safe --datadir=/var/lib/mysql &
+  usr/bin/mysqld_safe &
 
   # Wait until mariadb is ready to accept connections
   while ! mysqladmin ping -h "mariadb" --silent; do
@@ -30,8 +28,7 @@ else
 fi
 
 # Run the server
-mysqld_safe --datadir=/var/lib/mysql
-
+usr/bin/mysqld_safe
 
 #cat .exist 2> /dev/null
 #if [ $? -ne 0 ]; then
--- srcs/requirements/mariadb/conf/create_user.sql
+++ srcs/requirements/mariadb/conf/create_user.sql
@@ -5,15 +5,13 @@ DELETE FROM mysql.db WHERE Db='test';
 DELETE FROM mysql.user WHERE User='';
 DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
 
-UPDATE mysql.user SET password=PASSWORD('$DB_ROOT_PASSWORD') WHERE user='root';
-
 /** Setup mysql root password **/
-/*SET PASSWORD FOR 'root'@'localhost' = PASSWORD('$DB_ROOT_PASSWORD');
+SET PASSWORD FOR 'root'@'localhost' = PASSWORD('$DB_ROOT_PASSWORD');
 
 /** Create database and user for the project */
-CREATE DATABASE IF NOT EXISTS '$DB_DATABASE';
+CREATE DATABASE IF NOT EXISTS $DB_DATABASE;
 CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';
-GRANT ALL PRIVILEGES ON $DB_DATABASE.* TO $DB_USER@%;
+GRANT ALL PRIVILEGES ON $DB_DATABASE.* TO $DB_USER@'%';
 
 /** Apply changes **/
 FLUSH PRIVILEGES;

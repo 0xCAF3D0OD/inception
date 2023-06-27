98f8dcf9ab3c261db75053646f4e3ee41e70dfad
hack10101101 <kevin.dinocera@protonmail.com>
2023-06-27 16:39:27 +0200

    make mariadb access to users

--- srcs/requirements/mariadb/conf/create_user.sh
+++ srcs/requirements/mariadb/conf/create_user.sh
@@ -1,54 +1,44 @@
 #!/bin/sh
-
 # Cheking if the database is already created
-cat .exist 2>/dev/null
+#cat .exist 2>/dev/null
+#
+#if [ $? -ne 0 ]; then
+# # Run the server in the background and wait for it to start up before creating the database
+#  mysql_install_db
+#
+# # Give permissions to the database for the user mysql
+#  chown -R mysql:mysql /var/lib/mysql
+#  chown -R mysql:mysql /run/mysqld
+#
+#  # Start the server
+#  mysqld_safe &
+#
+#  # Wait until mariadb is ready to accept connections
+#  while ! mysqladmin ping -h "mariadb" --silent; do
+#    sleep 1
+#  done
+#
+#  # Create database from .sql file
+#  eval "echo \"\$(cat /tmp/create_user.sql)\"" | mariadb
+#  touch .exist
+#else
+#  echo "Database already created and ready"
+#fi
+#
+## Run the server
+#mysqld_safe
 
+cat .exist 2> /dev/null
 if [ $? -ne 0 ]; then
 
-  # Run the server in the background and wait for it to start up before creating the database
-  mysql_install_db 
+	usr/bin/mysqld_safe --datadir=/var/lib/mysql &
 
-  # Give permissions to the database for the user mysql
-  chown -R mysql:mysql /var/lib/mysql
-  chown -R mysql:mysql /run/mysqld
+	while ! mysqladmin ping -h "$MARIADB_HOST" --silent; do
+    	sleep 1
+	done
 
-  # Start the server
-  usr/bin/mysqld_safe &
-
-  # Wait until mariadb is ready to accept connections
-  while ! mysqladmin ping -h "mariadb" --silent; do
-    sleep 1
-  done
-
-  # Create database from .sql file
-  eval "echo \"\$(cat /tmp/create_user.sql)\"" | mariadb
-  touch .exist
+	eval "echo \"$(cat /tmp/create_user.sql)\"" | mariadb
+	touch .exist
 else
-  echo "Database already created and ready"
+	usr/bin/mysqld_safe --datadir=/var/lib/mysql
 fi
-
-# Run the server
-usr/bin/mysqld_safe
-
-#cat .exist 2> /dev/null
-#if [ $? -ne 0 ]; then
-#
-#	# Run the server in the background and wait for it to start up before creating the database
-#	mysql_install_db --datadir=/var/lib/mysql \
-#		--auth-root-authentication-method=normal
-#
-#	# Give permissions to the database for the user mysql
-#	chown -R mysql:mysql /var/lib/mysql
-#	chown -R mysql:mysql /run/mysqld
-#
-#	usr/bin/mysqld_safe --datadir=/var/lib/mysql &
-#
-#	while ! mysqladmin ping -h "$MARIADB_HOST" --silent; do
-#    	sleep 1
-#	done
-#
-#	eval "echo \"$(cat /tmp/create_user.sql)\"" | mariadb
-#	touch .exist
-#else
-#	usr/bin/mysqld_safe --datadir=/var/lib/mysql
-#fi

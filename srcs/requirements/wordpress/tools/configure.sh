42d3addffb9eee0bf32c88d5ad237141591d3e73
hack10101101 <kevin.dinocera@protonmail.com>
2023-07-05 16:47:53 +0200

    modify variable name .env

--- srcs/requirements/wordpress/tools/configure.sh
+++ srcs/requirements/wordpress/tools/configure.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # wait for mysql
-while ! mariadb -h$DB_HOST -u$DB_USER -p$DB_PASSWORD $DB_DATABASE &>/dev/null; do
+while ! mariadb -h$DB_HOST -u$DB_USER -p$DB_PWD $DB_NAME &>/dev/null; do
     sleep 3
 done
 
@@ -15,18 +15,19 @@ if [ ! -f "/var/www/html/index.html" ]; then
     wget https://raw.githubusercontent.com/Niyko/Hydra-Dark-Theme-for-Adminer/master/adminer.css -O /var/www/html/adminer.css &> /dev/null
 
     wp core download --allow-root
-    wp config create --dbname=$DB_DATABASE --dbuser=$DB_USER --dbpass=$DB_PASSWORD --dbhost=$DB_HOST --dbcharset="utf8" --dbcollate="utf8_general_ci" --allow-root
-    wp core install --url=$DOMAIN_NAME/wordpress --title=$WP_TITLE --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
-    wp user create $WP_USER $WP_USER_EMAIL --role=author --user_pass=$WP_USER_PWD --allow-root
+    wp config create --dbhost=$DB_HOST --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PWD --dbcharset="utf8" --dbcollate="utf8_general_ci" --allow-root
+    wp core install --url=/Users/$USER/data/wordpress --title=$WP_TITLE --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
+						#$DOMAIN_NAME/
+	wp user create $WP_USER $WP_USER_EMAIL --role=author --user_pass=$WP_USER_PWD --allow-root
     wp theme install inspiro --activate --allow-root
 
-    # enable redis cache
-    sed -i "40i define( 'WP_REDIS_HOST', '$REDIS_HOST' );"      wp-config.php
-    sed -i "41i define( 'WP_REDIS_PORT', 6379 );"               wp-config.php
-    #sed -i "42i define( 'WP_REDIS_PASSWORD', '$REDIS_PWD' );"   wp-config.php
-    sed -i "42i define( 'WP_REDIS_TIMEOUT', 1 );"               wp-config.php
-    sed -i "43i define( 'WP_REDIS_READ_TIMEOUT', 1 );"          wp-config.php
-    sed -i "44i define( 'WP_REDIS_DATABASE', 0 );\n"            wp-config.php
+#    # enable redis cache
+#    sed -i "40i define( 'WP_REDIS_HOST', '$REDIS_HOST' );"      wp-config.php
+#    sed -i "41i define( 'WP_REDIS_PORT', 6379 );"               wp-config.php
+#    #sed -i "42i define( 'WP_REDIS_PASSWORD', '$REDIS_PWD' );"   wp-config.php
+#    sed -i "42i define( 'WP_REDIS_TIMEOUT', 1 );"               wp-config.php
+#    sed -i "43i define( 'WP_REDIS_READ_TIMEOUT', 1 );"          wp-config.php
+#    sed -i "44i define( 'WP_REDIS_DATABASE', 0 );\n"            wp-config.php
 
     wp plugin install redis-cache --activate --allow-root
     wp plugin update --all --allow-root

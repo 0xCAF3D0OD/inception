faa6cb0274dff10677ecc2763b52aab5a1ef2008
kdi-noce <kdi-noce@keke.kdi-noce.42.fr>
2023-07-06 11:39:01 -0500

    almost finished

--- srcs/requirements/mariadb/conf/create_user.sh
+++ srcs/requirements/mariadb/conf/create_user.sh
@@ -30,8 +30,12 @@
 
 cat .exist 2> /dev/null
 if [ $? -ne 0 ]; then
-
-	usr/bin/mysqld_safe --datadir=/var/lib/mysql &
+        
+	mysql_install_db --datadir=/var/lib/mysql --auth-root-authentication-method=normal
+        chown -R mysql:mysql /var/lib/mysql
+        chown -R mysql:mysql /run/mysqld
+	
+	/usr/bin/mysqld_safe --datadir=/var/lib/mysql &
 
 	while ! mysqladmin ping -h "$MARIADB_HOST" --silent; do
     	sleep 1
@@ -39,6 +43,7 @@ if [ $? -ne 0 ]; then
 
 	eval "echo \"$(cat /tmp/create_user.sql)\"" | mariadb -u root -p12345
 	touch .exist
-else
-	usr/bin/mysqld_safe --datadir=/var/lib/mysql
+	exit
 fi
+
+exec /usr/bin/mysqld_safe --datadir=/var/lib/mysql
--- srcs/requirements/wordpress/tools/configure.sh
+++ srcs/requirements/wordpress/tools/configure.sh
@@ -10,13 +10,14 @@ done
 if [ ! -f "/var/www/html/index.html" ]; then
 
     # static website
-    mv /tmp/wp-config.php /var/www/html/wp-config.php
+    mv /tmp/wp-config.php /var/www/html/wordpress/wp-config.php
     mv /tmp/index.html /var/www/html/index.html
 
     # adminer
     wget https://github.com/vrana/adminer/releases/download/v4.8.1/adminer-4.8.1-mysql-en.php -O /var/www/html/adminer.php &> /dev/null
     wget https://raw.githubusercontent.com/Niyko/Hydra-Dark-Theme-for-Adminer/master/adminer.css -O /var/www/html/adminer.css &> /dev/null
 
+    cd /var/www/html/wordpress
     wp core download --allow-root
     wp config create --dbhost=$DB_HOST --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PWD --dbcharset="utf8" --dbcollate="utf8_general_ci" --allow-root
     wp core install --url=$DOMAIN_NAME --title=$WP_TITLE --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root

3b4307e5a008a02e31475f88fc6ef0bee5959e4d
0xCAF3D0OD <kevin.dinocera@protonmail.com>
2023-07-08 18:44:05 +0200

    eval 1

--- srcs/requirements/mariadb/conf/create_user.sh
+++ srcs/requirements/mariadb/conf/create_user.sh
@@ -1,55 +1,19 @@
-#!/bin/sh
-# Cheking if the database is already created
-#cat .exist 2>/dev/null
-#
-#if [ $? -ne 0 ]; then
-# # Run the server in the background and wait for it to start up before creating the database
-#  mysql_install_db
-#
-# # Give permissions to the database for the user mysql
-#  chown -R mysql:mysql /var/lib/mysql
-#  chown -R mysql:mysql /run/mysqld
-#
-#  # Start the server
-#  mysqld_safe &
-#
-#  # Wait until mariadb is ready to accept connections
-#  while ! mysqladmin ping -h "mariadb" --silent; do
-#    sleep 1
-#  done
-#
-#  # Create database from .sql file
-#  eval "echo \"\$(cat /tmp/create_user.sql)\"" | mariadb
-#  touch .exist
-#else
-#  echo "Database already created and ready"
-#fi
-#
-## Run the server
-#mysqld_safe
-
 cat .exist 2> /dev/null
 if [ $? -ne 0 ]; then
-	echo "1"        
+
 	mysql_install_db --datadir=/var/lib/mysql --auth-root-authentication-method=normal
-        echo "2"
 	chown -R mysql:mysql /var/lib/mysql
-        echo "3"
 	chown -R mysql:mysql /run/mysqld
 	
-        echo "4"
 	/usr/bin/mysqld_safe --datadir=/var/lib/mysql &
 
-        echo "5"
 	while ! mysqladmin ping -h "$MARIADB_HOST" --silent; do
     	sleep 1
 	done
 
-        echo "6"
 	eval "echo \"$(cat /tmp/create_user.sql)\"" | mariadb -u root -p12345
 	touch .exist
 	exit
 fi
 
-echo "7"
 exec /usr/bin/mysqld_safe --datadir=/var/lib/mysql
--- srcs/requirements/wordpress/tools/configure.sh
+++ srcs/requirements/wordpress/tools/configure.sh
@@ -9,46 +9,22 @@ done
 
 if [ ! -f "/var/www/html/index.html" ]; then
 
-	echo "7"
 	# static website
-	mv /tmp/index.html /var/www/html/index.html
+	mv /tmp/index.html /var/www/html/wordpress/index.html
 	
-	echo "8"
 	# adminer
-	wget https://github.com/vrana/adminer/releases/download/v4.8.1/adminer-4.8.1-mysql-en.php -O /var/www/html/adminer.php &> /dev/null
-	wget https://raw.githubusercontent.com/Niyko/Hydra-Dark-Theme-for-Adminer/master/adminer.css -O /var/www/html/adminer.css &> /dev/null
+	wget https://github.com/vrana/adminer/releases/download/v4.8.1/adminer-4.8.1-mysql-en.php -O /var/www/html/wordpress/adminer.php &> /dev/null
+	wget https://raw.githubusercontent.com/Niyko/Hydra-Dark-Theme-for-Adminer/master/adminer.css -O /var/www/html/wordpress/adminer.css &> /dev/null
 	
 	cd /var/www/html/wordpress
-	echo "9"
 	wp core download --allow-root
-	echo "10"	
 	wp config create --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PWD --dbhost=$DB_HOST --dbprefix=wp_ --dbcharset="utf8" --dbcollate="utf8_general_ci" --extra-php --skip-salts --skip-check --force --insecure
-#	wp config create --dbhost=$DB_HOST --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PWD --dbcharset="utf8" --dbcollate="utf8_general_ci" --allow-root --force
-	echo "11"
 	wp core install --url=$DOMAIN_NAME --title=$WP_TITLE --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
-	    wp user create $WP_USER $WP_USER_EMAIL --role=author --user_pass=$WP_USER_PWD --allow-root
-	echo "12"
+	wp user create $WP_USER $WP_USER_EMAIL --role=author --user_pass=$WP_USER_PWD --allow-root
 	wp theme install inspiro --activate --allow-root
-	echo "13"
 	ls -la /var/www/html/wordpress 
 
-	echo "14"
-	# enable redis cache
-	#sed -i "40i define( 'WP_REDIS_HOST', '$REDIS_HOST' );"      wp-config.php
-	#sed -i "41i define( 'WP_REDIS_PORT', 6379 );"               wp-config.php
-	##sed -i "42i define( 'WP_REDIS_PASSWORD', '$REDIS_PWD' );"   wp-config.php
-	#sed -i "42i define( 'WP_REDIS_TIMEOUT', 1 );"               wp-config.php
-	#sed -i "43i define( 'WP_REDIS_READ_TIMEOUT', 1 );"          wp-config.php
-	#sed -i "44i define( 'WP_REDIS_DATABASE', 0 );\n"            wp-config.php
-	#
-	#echo "15"
-	#wp plugin install redis-cache --activate --allow-root
-	#wp plugin update --all --allow-root
-
 fi
-
-#echo "12"
-#wp redis enable --allow-root
 
 echo "Wordpress started on :9000"
 /usr/sbin/php-fpm7 -F -R
